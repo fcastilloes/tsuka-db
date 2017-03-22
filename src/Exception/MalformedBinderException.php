48da661c86354728972682901e14d36c64e65d39
Ricardo Vegas <ricardo.vegas@cakedc.com>
2017-03-22 14:04:00 +0100

    Remove unnecessary constructor

--- src/Exception/MalformedBinderException.php
+++ src/Exception/MalformedBinderException.php
@@ -10,7 +10,4 @@ namespace Tsuka\DB\Exception;
 
 class MalformedBinderException extends \Exception
 {
-    public function __construct($message, $code = 0, \Exception $previous = null) {
-        parent::__construct($message, $code, $previous);
-    }
 }
